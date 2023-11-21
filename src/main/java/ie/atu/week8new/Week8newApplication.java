ab04a16e3389debc4968377fb8604cb71dfe643b
G00403720@atu.ie <G00403720@atu.ie>
2023-11-21 16:01:51 +0000

    Week8

---
+++ src/main/java/ie/atu/week8new/Week8newApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week8new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week8newApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week8newApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week8new/Week8newApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week8new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week8newApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
